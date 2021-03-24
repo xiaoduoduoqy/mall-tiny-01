b37a1c7aac0a265bd494db84b8fffcca387ed5c2
钱咏 <492300924@qq.com>
2021-03-24 21:15:14 +0800

    添加插件Lombok依赖

--- src/main/java/com/macro/mall/tiny/component/OrderTimeOutCancelTask.java
+++ src/main/java/com/macro/mall/tiny/component/OrderTimeOutCancelTask.java
@@ -20,7 +20,7 @@ public class OrderTimeOutCancelTask {
      * cron表达式：Seconds Minutes Hours DayofMonth Month DayofWeek [Year]
      * 每10分钟扫描一次，扫描设定超时时间之前下的订单，如果没支付则取消该订单
      */
-    @Scheduled(cron = "0/5 * * * * ? ")
+    @Scheduled(cron = "0 0/10 * * * ? ")
     private void cancelTimeOutOrder() {
         // TODO: 2019/5/3 此处应调用取消订单的方法，具体查看mall项目源码
         LOGGER.info("取消订单，并根据sku编号释放锁定库存");
